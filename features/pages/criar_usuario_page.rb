f715a4877577671952cbd8d758fd8cf8ffa59170
Jilvan <jilvan.pereira@gold360.com.br>
2018-09-14 15:26:11 -0300

    Projeto Final

--- features/pages/criar_usuario_page.rb
+++ features/pages/criar_usuario_page.rb
@@ -1,13 +1,13 @@
 class User < SitePrism::Page
     set_url 'users/new'
-    element :nome, 'user_name'
-    element :sobrenome, 'user_lastname'
-    element :email, 'user_email'
-    element :endereco, 'user_address'
-    element :universidade, 'user_university'
-    element :profissao, 'user_profile'
-    element :genero, 'user_gender'
-    element :idade, 'user_age'
+    element :nome, '#user_name'
+    element :sobrenome, '#user_lastname'
+    element :email, '#user_email'
+    element :endereco, '#user_address'
+    element :universidade, '#user_university'
+    element :profissao, '#user_profile'
+    element :genero, '#user_gender'
+    element :idade, '#user_age'
     element :criar, 'input[value="Criar"]'
 
     def preencher_usuario
@@ -15,9 +15,10 @@ class User < SitePrism::Page
         sobrenome.set 'Junior'
         email.set 'jilvan@junior.com'
         endereco.set 'Rua Elvira Ferraz'
-        profissao.set 'Anhanguera'
+        universidade.set 'Anhanguera' 
+        profissao.set 'Analista'
         genero.set 'Masculino'
-        idade.set '23'
+        idade.set '22'
         criar.click
     end
 
