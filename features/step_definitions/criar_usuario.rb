b796e1acfbe83fe6f5a5fba8fe6ea2e0ae104185
Jilvan <jilvan.pereira@gold360.com.br>
2018-09-14 09:21:30 -0300

    teste

--- features/step_definitions/criar_usuario.rb
+++ features/step_definitions/criar_usuario.rb
@@ -2,9 +2,10 @@ Quando("eu cadastro meu usuário") do
     user.load
     user.preencher_usuario
 
-    sleep(2)
+    sleep(3)
   end                                                                           
                                                                                 
   Então("eu verifico se o usuário foi cadastrado") do                           
     
-  end                                                                           
+  end   
+                                                                          
